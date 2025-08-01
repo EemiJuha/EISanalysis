a7da36352b1162bdd41d095b09ed9bbd2d4d8cc0
Eemi Nieminen <eemi.nieminen@gmail.com>
2025-08-01 13:35:06 +0200

    learning matpotlib and pandas

--- plotter.py
+++ plotter.py
@@ -9,10 +9,11 @@ Created on Wed Jul 30 15:15:15 2025
 
 import pandas as pd
 import tkinter.filedialog
-import matplotlib as mpl
-fileNameList = tkinter.filedialog.askopenfilenames()
 
-exampleFile = fileNameList[0]
+import matplotlib.pyplot as plt
+#fileNameList = tkinter.filedialog.askopenfilenames()
+dummyFile = 'C:/Users/eemin/OneDrive/Tiedostot/Python Scripts/SMS-horiba2025/20250429-CV-measurements-1mMRuHex-again both directions/CVOx200mVs.txt'
+exampleFile = dummyFile
 startrow = 0
 with open(exampleFile, mode='r') as file:
     for num, line in enumerate(file, 1):
@@ -21,3 +22,7 @@ with open(exampleFile, mode='r') as file:
             break
 
 exampleTab = pd.read_table(exampleFile, sep=',', skiprows=startrow)
+
+figure = plt.subplot()
+plt.plot(exampleTab.ilog[:,0],exampleTab.iloc[:,1])
+
